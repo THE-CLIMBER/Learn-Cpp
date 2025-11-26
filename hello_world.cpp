5df9546c4481dc9440b1dd3dc658a78a0c0fae91
THE_CLIMBER <datalogiewithmo@gmail.com>
2025-11-26 01:41:46 +0100

    Starting

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 42;
+    int y = x * 2;
+    int z = y - 10;
+    std::cout << x << " " << y << " " << z << '\n';
+    std::string message = "Hello, world!";
+    std::cout << message << std::endl;
+    return 0;
+}
